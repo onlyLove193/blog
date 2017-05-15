3f4f2a31655a182df83dfa8b437ac10622c014d2
onlyLove193 <onlyLove193@qq.com>
2017-05-15 22:57:48 +0800

    s

--- App/Admin/Controller/CateAddController.class.php
+++ App/Admin/Controller/CateAddController.class.php
@@ -6,17 +6,29 @@ class CateAddController extends CommonController
 	public function index()
 	{
 		$cat = M('category')->field(array('gid','cname','pid'))->select();
-		// $cat = getRow($cat);
+		$this->cat = getRow($cat);
 		
-		
-		p($cat);exit;
-		// $this->title = '添加栏目';
-		// $this->navi = ['Clist'=>'栏目模块','CateAdd'=>'添加栏目'];
-		// $this->display();
+		$this->title = '添加栏目';
+		$this->navi = ['Clist'=>'栏目模块','CateAdd'=>'添加栏目'];
+		$this->display();
 	}
 
-	protected function Multi_tree()
+	public function addHandle()
 	{
+		if(!IS_POST){
+			$this->error('页面不存在','index',5);
+			return;
+		}
+		$data = [];
+		$data['cname'] = I('post.cname','','htmlspecialchars');
+		$data['pid'] = I('post.pid','0','htmlspecialchars');
+		$data['descript'] = I('post.descript','','htmlspecailchars');
+		$data['keywords'] = I('post.keywords','','htmlspecialchars');
+		$data['status'] = I('post.status','0','htmlspecialchars');
 
-	}
+		$res = M('category')->add($data);
+		if($res) $this->success('栏目添加成功！',U('Clist/index'),3);
+		else $this->error('栏目添加失败！',U('index'),5);
+ 	}
+
 }
--- App/Admin/Controller/CateManageController.class.php
+++ App/Admin/Controller/CateManageController.class.php
@@ -5,6 +5,31 @@ class CateManageController extends CommonController
 {
 	public function index()
 	{
+		$clist = M('category');
+		$count = $clist->count();
+		$page = getpage($count,10);
+		$show = $page->show();
+		$this->list = $clist->field(['descript','keywords'],true)->order('gid asc')->limit($page->firstRow.' , '.$page->listRows)->select();
+		$this->assign('page',$show);
+		$this->title = '栏目列表';
+		$this->navi = ["Clist"=>'栏目模块','栏目列表'];
+		$this->display();
+	}
+	public function cshow()
+	{
+		$gid = I('get.gid','','htmlspecialchars');
+		if(!IS_GET || !is_numeric($gid)) {
+			$this->error('页面不存在！',U('index'),5);
+			return ;
+		}
+		$category = M('category');
+		$this->res = $category->where(['gid'=>$gid])->find();
+
+		$cat = $category->field(array('gid','cname','pid'))->select();
+		$this->cat = getRow($cat);
+		
+		$this->title = '编辑栏目';
+		$this->navi = ['Clist'=>'栏目模块','CateAdd'=>'编辑栏目'];
 		$this->display();
 	}
 }
